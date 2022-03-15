94563b2ee342b757e704e5fe3d6d14840c9381d1
Alina <alinavik93@gmail.com>
2022-03-15 22:30:11 +0500

    commit

--- src/com/company/menu/MenuStart.java
+++ src/com/company/menu/MenuStart.java
@@ -1,6 +1,6 @@
 package com.company.menu;
 
-import com.company.Basket;
+import com.company.Category;
 import com.company.data.AppData;
 import com.company.Product;
 import com.company.User;
@@ -9,7 +9,6 @@ import com.company.helper.ScannerHelper;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 
 public class MenuStart {
@@ -18,27 +17,18 @@ public class MenuStart {
             + "\n\t2 - Authorization"
             + "\n\t0 - Exit";
     private static final String VIEWING_PRODUCTS = "\n\t1 - Go back to selecting categories"
-            + "\n\t2 - Go back to the main menu"
             + "\n\t0 - Exit the program";
 
     public static final String INCORRECT = "THERE IS NO SUCH ACTION!!!";
     public static final String SELECT_ACTION = "\nSELECT AN ACTION:\n ";
 
-    private static int NUM_SELECT_CATEGORY;
 
-    public MenuStart(ArrayList<User> initializeFromFile) {
-    }
-
-    public MenuStart() {
-    }
-
-    public static void startMenuWhoIsNotLogged(ArrayList<User> users) {
+    public static void startMenuWhoIsNotLogged(ArrayList <User> users, ArrayList<Category> categories) {
         boolean itContinues = true;
-//        ResourceBundle bundleDefault = ResourceBundle.getBundle("resources", Locale.getDefault());
         while (itContinues) {
             switch (ScannerHelper.getIntFromInput(START_MENU + SELECT_ACTION)) {
                 case 1:
-                    viewProduct(users);
+                    viewProduct(categories);
                     break;
                 case 2:
                     MenuAuthorization.startMenuAuthentication(users);
@@ -54,46 +44,44 @@ public class MenuStart {
     }
 
 
-    private static void viewCategory() {
+    private static void viewCategory(ArrayList<Category> categories) {
         System.out.println("Product categories:");
         System.out.format("+----+---------------------+%n");
         System.out.format("| №  |  Categories         |%n");
         System.out.format("+----+---------------------+%n");
-        for (int i = 0; i < AppData.categories.size(); i++) {
-            System.out.format("%-5s%-22s%1s", "| " + (i + 1) + ".", "| " + AppData.categories.get(i).getName(), "|\n");
+        for (int i = 0; i < categories.size(); i++) {
+            System.out.format("%-5s%-22s%1s", "| " + (i + 1) + ".", "| " + categories.get(i).getName(), "|\n");
         }
         System.out.format("+----+---------------------+");
         System.out.println("\n");
     }
 
-    private static void selectCategory(int num) {
-        viewCategory();
+    private static int selectCategory(ArrayList<Category> categories) {
+        viewCategory(categories);
         System.out.println("Enter the category number to view the products:\n");
-        NUM_SELECT_CATEGORY = ScannerHelper.readInt() - 1;
+        int num = ScannerHelper.readInt() - 1;
+        return num;
     }
 
-    private static void viewProduct(ArrayList <User> users) {
-        selectCategory(NUM_SELECT_CATEGORY);
+    private static void viewProduct(ArrayList<Category> categories) {
+        ArrayList<Product> products = categories.get(selectCategory(categories)).getProducts();
         System.out.println("List of products:");
-        ArrayList<Product> products = AppData.categories.get(NUM_SELECT_CATEGORY).getProducts();
         System.out.format("+----+---------------------+-------------+---------+%n");
         System.out.format("| №  |  Product            | Price       | Rating  |%n");
         System.out.format("+----+---------------------+-------------+---------+%n");
         for (int i = 0; i < products.size(); i++) {
-            System.out.format("%-5s%-22s%-14s%-10s%1s", "| " + (i + 1) + ".", "| " + products.get(i).getName(), "| " + NumberFormat.getCurrencyInstance(Locale.getDefault()).format(products.get(i).getPrice()), "| " + products.get(i).getRating(), "|\n");
+            System.out.format("%-5s%-22s%-14s%-10s%1s", "| " + (i + 1) + ".", "| " + products.get(i).getName(), "| "
+                    + NumberFormat.getCurrencyInstance(Locale.getDefault()).format(products.get(i).getPrice()), "| "
+                    + products.get(i).getRating(), "|\n");
         }
         System.out.format("+----+---------------------+-------------+---------+");
-        System.out.println("\n");
-        choosingActionsAfterViewProduct(users);
+        choosingActionsAfterViewProduct(categories);
     }
 
-    private static void choosingActionsAfterViewProduct (ArrayList <User> users){
+    private static void choosingActionsAfterViewProduct (ArrayList<Category>categories){
         switch (ScannerHelper.getIntFromInput(VIEWING_PRODUCTS + SELECT_ACTION)) {
             case 1:
-                viewProduct(users);
-                break;
-            case 2:
-                startMenuWhoIsNotLogged(users);
+                viewProduct(categories);
                 break;
             case 0:
                 System.exit(0);
---
+++ src/com/company/saving/StartInit.java
@@ -0,0 +1,23 @@
+package com.company.saving;
+
+import com.company.Category;
+import com.company.User;
+import com.company.data.AppData;
+
+import java.util.ArrayList;
+
+import static com.company.menu.MenuStart.startMenuWhoIsNotLogged;
+
+public class StartInit {
+    public final static String fileUser = "src/com/company/data/Users.txt";
+    public final static String fileCategory = "src/com/company/data/Category.txt";
+
+    public static void startInit() {
+
+        ArrayList<User> users = InitializeFromFile.initializeFromFile(fileUser);
+        ArrayList<Category> categories = InitializeFromFile.initializeFromFile(fileCategory);
+
+        startMenuWhoIsNotLogged(users, categories); //передача юзер+категория
+    }
+
+}
